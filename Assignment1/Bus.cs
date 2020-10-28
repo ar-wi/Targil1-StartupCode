30bca420099eb6af0e60c1d8542f2308aeb954b5
aryehJCT <33257705+aryehJCT@users.noreply.github.com>
2020-10-28 22:03:15 +0200

    Add project files.

---
+++ Assignment1/Bus.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Assignment1
+{
+    public class Bus
+    {
+        const int FULLTANK = 1200;
+        public readonly DateTime StartDate;
+        private string license;
+        private int km;
+
+        public int Km { get => km; set => km = value; }
+        public int Fuel { get; set; }
+        public DateTime Checkup { get; private set; }
+
+        public Bus()
+        {
+            Console.Write("Starting date: ");
+            bool result = DateTime.TryParse(Console.ReadLine(), out StartDate);
+            if (result == false)
+            {
+                throw new Exception("invalid DateTime string format");
+            }
+            Console.Write("License number: ");
+            License = Console.ReadLine();
+        } 
+
+        public string License 
+        {   get
+            {
+                string first, middle, last;
+                if (license.Length == 7)
+                {
+                    // xx-xxx-xx
+                    first = license.Substring(0, 2);
+                    middle = license.Substring(2, 3);
+                    last = license.Substring(5, 2);
+                    return string.Format("{0}-{1}-{2}", first, middle, last);
+                }
+                else
+                {
+                    // xxx-xx-xxx
+                    first = license.Substring(0, 3);
+                    middle = license.Substring(3, 2);
+                    last = license.Substring(5, 3);
+                    return string.Format("{0}-{1}-{2}", first, middle, last);
+                }
+            }
+
+            private set
+            {
+                if ((StartDate.Year < 2018 && value.Length == 7) || (StartDate.Year >= 2018 && value.Length == 8))
+                {
+                    for (int i = 0; i < value.Length; i++)
+                    {
+                        if (value[i] < '0' || value[i] > '9')
+                            throw new Exception("license not valid");
+                    }
+                    license = value;
+                }
+                else
+                {
+                    throw new Exception("license not valid");
+                }
+            }
+        }
+
+        public int Refuel()
+        {
+            int fuelToAdd = FULLTANK - Fuel;
+
+            if (fuelToAdd > 0)
+            {
+                Fuel = FULLTANK;
+                return fuelToAdd;
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("license: {0,-10}, starting date: {1}, km: {2}", License, StartDate.Date, Km);
+        }
+
+    }
+
+}
---
+++ Assignment1/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Actions action;
+            List<Bus> buses = new List<Bus>();
+//            string license;
+            bool result;
+
+            do
+            {
+                Console.WriteLine("Choose an action from:");
+                foreach (Actions act in (Actions[])Enum.GetValues(typeof(Actions)))
+                {
+                    Console.WriteLine("\t" + act);
+                }
+                Console.Write("\nYour choice: ");
+                result = Enum.TryParse(Console.ReadLine(), out action);
+                if (!result)
+                {
+                    Console.WriteLine("no such option\n");
+                    continue;
+                }
+
+                switch (action) 
+                {
+                    case Actions.ADD:
+                        try
+                        {
+                            buses.Add(new Bus());
+                        }
+                        catch (Exception exception)
+                        {
+                            Console.WriteLine(exception.Message);
+                        }
+
+                        foreach (Bus bus in buses)
+                        {
+                            Console.WriteLine(bus);
+                        }
+
+                        break;
+
+                    case Actions.FIND:
+                        break;
+                    case Actions.REFUEL:
+                        break;
+                    default:
+                        break;
+                }
+
+            } while (action != Actions.EXIT);
+
+        }
+
+        private static void RefuelBus(List<Bus> buses, string license)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void FindBus(List<Bus> buses, string license)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+
+}
